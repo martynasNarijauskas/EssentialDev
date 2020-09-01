f2fbd42c69df568f04053230f8e231b09c8097d1
martynasnarijauskas <martynas.narijauskas@vinted.com>
2020-09-01 21:34:25 +0300

    Initialise project

---
+++ FeedApp/FeedApp.h
@@ -0,0 +1,19 @@
+//
+//  FeedApp.h
+//  FeedApp
+//
+//  Created by Martynas Narijauskas on 2020-09-01.
+//  Copyright © 2020 Martynas Narijauskas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedApp.
+FOUNDATION_EXPORT double FeedAppVersionNumber;
+
+//! Project version string for FeedApp.
+FOUNDATION_EXPORT const unsigned char FeedAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedApp/PublicHeader.h>
+
+
---
+++ FeedApp/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  FeedApp
+//
+//  Created by Martynas Narijauskas on 2020-09-01.
+//  Copyright © 2020 Martynas Narijauskas. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
